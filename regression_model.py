4efae776402633113d4338b951c92955551f0891
Vanskarner <vanskarner@gmail.com>
2024-02-07 18:01:54 -0500

    updated

--- regression_model.py
+++ regression_model.py
@@ -1,10 +1,18 @@
 """ modelo aa para predecir la conversion de Celsius a Fahrenheit """
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 # Data
-celsius = [-40, -10, 0, 8, 15, 22]
-fahrenheit = [-40, 14, 32, 46.4, 59, 71.6]
-dataForTest = [100, 200, 300]
+dataset = {
+    'train': {
+        'x_axis': [-40, -10, 0, 8, 15, 22],
+        'y_axis': [-40, 14, 32, 46.4, 59, 71.6]
+    },
+    'test': {
+        'x_axis': [100, 200, 300],
+        'y_axis': [212, 392, 572]
+    }
+}
 
 # Capas
 entry_layer = tf.keras.layers.Flatten(input_shape=[1])
@@ -19,17 +27,25 @@ model.compile(
     optimizer=tf.optimizers.Adam(0.09),
     loss=tf.losses.MeanSquaredError()
 )
-training_result = model.fit(x=celsius, y=fahrenheit, epochs=150, verbose=False)
+TRAINING_RESULT = model.fit(x=dataset['train']['x_axis'], y=dataset['train']['y_axis'], epochs=150, verbose=False)
 
 # Predicción
-predictions = model.predict(x=dataForTest, use_multiprocessing=True)
+predictions = model.predict(x=dataset['test']['x_axis'], use_multiprocessing=True)
 print(predictions)
 
 # ------------ Graficas ------------
-import matplotlib.pyplot as plt
 
 plt.title('Evolución de la pérdida durante el entrenamiento')
 plt.xlabel('# Epocas')
 plt.ylabel('# Magnitud de perdida')
-plt.plot(training_result.history['loss'])
+plt.plot(TRAINING_RESULT.history['loss'])
+plt.show()
+
+plt.title('Precisión del modelo')
+plt.xlabel('Celsius')
+plt.ylabel('Fahrenheit')
+plt.scatter(dataset['test']['x_axis'], predictions, color='red',
+            label='Predicciones del modelo')
+plt.plot(dataset['test']['x_axis'], dataset['test']['y_axis'], 'bo-', label='Valores reales')
+plt.legend()
 plt.show()
