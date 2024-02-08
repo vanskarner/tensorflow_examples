b6a59bdcdc57d65ba21e1fb9fde89b204f73f220
Vanskarner <vanskarner@gmail.com>
2024-02-08 10:36:24 -0500

    finalizacion for model 'celsius to fahrenheit'

--- regresion/celsius_fahrenheit/training.py
+++ regresion/celsius_fahrenheit/training.py
@@ -1,4 +1,4 @@
-""" modelo aa para predecir la conversión de Celsius a Fahrenheit """
+""" Entrenamiento del modelo para la conversión de Celsius a Fahrenheit """
 import os
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -7,13 +7,13 @@ import tensorflow as tf
 celsius_train = [-40, -10, 0, 8, 15, 22]
 fahrenheit_train = [-40, 14, 32, 46.4, 59, 71.6]
 
-# Capas
+# Creacion de capas
 entry_layer = tf.keras.layers.Flatten(input_shape=[1])
 hidden_layer1 = tf.keras.layers.Dense(units=3)
 hidden_layer2 = tf.keras.layers.Dense(units=3)
 output_layer = tf.keras.layers.Dense(units=1)
 
-# Modelo
+# Creacion del Modelo a partir de las capas
 layers = [entry_layer, hidden_layer1, hidden_layer2, output_layer]
 model = tf.keras.Sequential(layers, name='celsius_fahrenheit')
 model.compile(
@@ -32,7 +32,7 @@ MODEL_NAME = "CelToFah.keras"
 filepath = os.path.join(path, MODEL_NAME)
 model.save(filepath=filepath)
 
-# ------------ Graficas ------------
+# ------------ Grafica del entrenamiento ------------
 plt.title('Evolución de la pérdida durante el entrenamiento')
 plt.xlabel('# Epocas')
 plt.ylabel('# Magnitud de perdida')
