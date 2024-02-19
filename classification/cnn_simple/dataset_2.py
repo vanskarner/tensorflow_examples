caec25e612e2c9b956b39381600ed5a3ee5ddd9f
Vanskarner <vanskarner@gmail.com>
2024-02-18 21:17:00 -0500

    ways to display data for cnn

--- classification/cnn_simple/dataset_2.py
+++ classification/cnn_simple/dataset_2.py
@@ -9,22 +9,22 @@ data, metadata = tfds.load(
     as_supervised=True,
     with_info=True,
     download=False)
-test_data = cast(tf.data.Dataset, data['test'])
+train_data = cast(tf.data.Dataset, data['train'])
 categories = metadata.features['label'].names
 
 # ------------ Mostrar primer elemento ------------
 plt.suptitle('First Image')
-for first_image, first_label_index in test_data.take(1):
+for first_image, first_label_index in train_data.take(1):
     plt.imshow(X=first_image)
     plt.xlabel(xlabel=categories[first_label_index])
     plt.colorbar()
     plt.grid(False)
     plt.show()
 
-# ------------ Mostrar varios elemento ------------
+# ------------ Mostrar varios elementos ------------
 plt.figure(figsize=(10, 10))
 plt.suptitle('Several Images')
-for i, (image, label_index) in enumerate(test_data.take(25)):
+for i, (image, label_index) in enumerate(train_data.take(25)):
     plt.subplot(5, 5, i+1)
     plt.xticks([])
     plt.yticks([])
