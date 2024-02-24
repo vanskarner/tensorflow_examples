8670e16a41c5a33c68c09c5c52887c898ffdf813
Vanskarner <vanskarner@gmail.com>
2024-02-24 15:48:06 -0500

    sorting code

--- classification/clothes/predictions.py
+++ classification/clothes/predictions.py
@@ -4,36 +4,46 @@ from typing import cast
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-import tensorflow_datasets as tfds # pylint: disable=C0411
+import tensorflow_datasets as tfds  # pylint: disable=C0411
+
+# Cargar modelo guardado
+MODEL_NAME = "ClothingClassifier.keras"
+path = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(path, MODEL_NAME)
+model: tf.keras.Sequential = tf.keras.models.load_model(filepath=filepath)
+model.summary()
 
 # Preparación de data
 data, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
-data_test = cast(tf.data.Dataset, data['test'])
-categories_tests = cast(list[str], metadata.features['label'].names)
+test_data = cast(tf.data.Dataset, data['test'])
+categories_tests = metadata.features['label'].names
+test_data = test_data.map(map_func=lambda image, label: (
+    tf.cast(image, tf.float32)/255, label))
 LOTSIZE = 25
-data_test = data_test.map(
-    map_func=lambda image, label: (tf.cast(image, tf.float32)/255, label)
-).batch(LOTSIZE)
-
-# Carga de modelo guardado
-path = os.path.dirname(os.path.abspath(__file__))
-MODEL_NAME = "ClothingClassifier.keras"
-filepath = os.path.join(path, MODEL_NAME)
-model: tf.keras.Sequential = tf.keras.models.load_model(filepath=filepath)
+num_examples = metadata.splits["test"].num_examples
+test_data = test_data.cache().shuffle(num_examples).batch(LOTSIZE)
 
 # Ejecución de predicciones
-firstBatch = next(iter(data_test))
-images_test, labels_test = firstBatch
-images_test = images_test.numpy()
-labels_test = labels_test.numpy()
+primerLote = test_data.take(1).as_numpy_iterator()
+images_test, labels_test = next(primerLote)
 predicctions = model.predict(images_test)
 
-# ------------ Gráfica de la predicción ------------
-ROWS = 5
-COLUMNS = 5
+# for images_test, labels_test in test_data.take(1):
+#     images_test = images_test.numpy()
+#     labels_test = labels_test.numpy()
+#     predicctions = model.predict(images_test)
+
+# firstBatch = next(iter(test_data))
+# images_test, labels_test = firstBatch
+# images_test = images_test.numpy()
+# labels_test = labels_test.numpy()
+# predicctions = model.predict(images_test)
+
+#------------ Gráfica de la predicción ------------
+ROWS, COLUMNS = (5, 5)
 NUM_IMAGES = ROWS*COLUMNS
 plt.figure(figsize=(2*2*COLUMNS, 2*ROWS))
-plt.suptitle('Resultado de predicciones')
+plt.suptitle('Predicciones')
 for i in range(NUM_IMAGES):
     # Gráfico de la imagen y etiquetas
     plt.subplot(ROWS, 2*COLUMNS, 2*i+1)
@@ -61,6 +71,7 @@ for i in range(NUM_IMAGES):
     # Agregar valores a las barras solo para predicted_label y real_label
     for j, val in enumerate(prediction):
         if j == predicted_label or j == real_label:
-            plt.text(j, val, f'{val:.2f}', ha='center', va='bottom', color='black')
+            plt.text(j, val, f'{val:.2f}', ha='center',
+                     va='bottom', color='black')
 
 plt.show()
