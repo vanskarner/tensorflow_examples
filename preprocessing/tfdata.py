fb8620f2a03ad81983224e44f4b9da5f52f2b2b8
Vanskarner <vanskarner@gmail.com>
2024-02-26 16:41:41 -0500

    update for preprocessing

--- preprocessing/tfdata.py
+++ preprocessing/tfdata.py
@@ -1,5 +1,5 @@
 """ 
-Carga y preprocesamiento de imagenes
+Carga y preprocesamiento de imágenes
 Usando tf.data
 """
 import os
@@ -8,52 +8,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file(origin=dataset_url,
-                                   fname='flower_photos',
-                                   untar=True)
+# ------------------------- OBTENER LA DATA -------------------------
+# Descarga el archivo flower_photos.tgz desde la URL proporcionada
+URL = 'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz'
+data_dir = tf.keras.utils.get_file(
+    origin=URL,
+    fname='flower_photos',
+    untar=True)
+
+# Convierte el directorio de datos a un objeto pathlib.Path para facilitar su manipulación.
 data_dir = pathlib.Path(data_dir)
 
-image_count = len(list(data_dir.glob('*/*.jpg')))  # 3670
+# Cuenta el número total de imágenes en el directorio contando todos
+# los archivos .jpg en todas las subcarpetas.
+image_count = len(list(data_dir.glob('*/*.jpg')))
 
+# Crea un array que contiene los nombres de todas las carpetas dentro del directorio
+# excluyendo el elemento con el nombre "LICENSE.txt"
+alguniterable = data_dir.glob('*')
+names = [item.name for item in alguniterable if item.name != "LICENSE.txt"]
+categories = np.array(sorted(names))
+
+# Diccionario con la información relevante
+info_data = {
+    'data_dir': data_dir,
+    'image_count': image_count,
+    'categories': categories
+}
+# ------------------------- PREPARACIÓN DE DATASET -------------------------
 batch_size = 32
 img_height = 180
 img_width = 180
 
 AUTOTUNE = tf.data.AUTOTUNE
 
-num_classes = 5
-
-# -------- Uso de tf.data --------
-list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'), shuffle=False)
-list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
-
-for f in list_ds.take(5):
-    # Muestra la ruta de 5 imagenes que son elegidas al azar, porque se uso shuffle
-    print(f.numpy())
-
-class_names = np.array(sorted(
-    [item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"]))
-# cuenta todos los elementos contenido en el directorio excepto LICENSE, por lo que nombra los nombre de las categorias
-print('algo', class_names)
+# list_ds contiene todas las rutas de las imagenes, por lo que hay 3670 rutas en un objeto tensorflow
+list_ds = tf.data.Dataset.list_files(
+    str(info_data['data_dir']/'*/*'), shuffle=False)
+list_ds = list_ds.shuffle(
+    info_data['image_count'], reshuffle_each_iteration=False)
 
 # Division del dataset en entrenamiento y validacion(test)
-val_size = int(image_count * 0.2)  # El 20% de 3670 es 734
-# El conjunto de entrenamiento evita la cantidad de 734 elementos
+val_size = int(info_data['image_count'] * 0.2)
+# El dataset de entrenamiento tomara los elementos despues de los primeros 734 elementos
 train_ds = list_ds.skip(val_size)
-# el conjunto de validacion de pruebas toma la cantida de 734 elementos
+# El dataset de validacion toma los primeros 734 elementos
 val_ds = list_ds.take(val_size)
-
-print('aa', tf.data.experimental.cardinality(
-    train_ds).numpy())  # entrenaminto 2936
-print('bb', tf.data.experimental.cardinality(val_ds).numpy())  # 734
 
 
 def get_label(file_path):
     # Convert the path to a list of path components
     parts = tf.strings.split(file_path, os.path.sep)
     # The second to last is the class-directory
-    one_hot = parts[-2] == class_names
+    one_hot = parts[-2] == info_data['categories']
     # Integer encode the label
     return tf.argmax(one_hot)
 
@@ -73,13 +80,10 @@ def process_path(file_path):
     return img, label
 
 
+# Use Dataset.map para crear un conjunto de datos de pares de image, label :
 # Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
 train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-
-for image, label in train_ds.take(1):
-    print("Image shape: ", image.numpy().shape)
-    print("Label: ", label.numpy())
 
 
 def configure_for_performance(ds):
@@ -94,36 +98,36 @@ train_ds = configure_for_performance(train_ds)
 val_ds = configure_for_performance(val_ds)
 
 image_batch, label_batch = next(iter(train_ds))
-
 plt.figure(figsize=(10, 10))
 for i in range(9):
     ax = plt.subplot(3, 3, i + 1)
     plt.imshow(image_batch[i].numpy().astype("uint8"))
     label = label_batch[i]
-    plt.title(class_names[label])
+    # plt.title(categories[label])
+    plt.title(info_data['categories'][label])
     plt.axis("off")
 plt.show()
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Rescaling(1./255),
-    tf.keras.layers.Conv2D(32, 3, activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(32, 3, activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(32, 3, activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(num_classes)
-])
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Rescaling(1./255),
+#     tf.keras.layers.Conv2D(32, 3, activation='relu'),
+#     tf.keras.layers.MaxPooling2D(),
+#     tf.keras.layers.Conv2D(32, 3, activation='relu'),
+#     tf.keras.layers.MaxPooling2D(),
+#     tf.keras.layers.Conv2D(32, 3, activation='relu'),
+#     tf.keras.layers.MaxPooling2D(),
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(128, activation='relu'),
+#     tf.keras.layers.Dense(len(class_names))
+# ])
 
-model.compile(
-    optimizer='adam',
-    loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'])
+# model.compile(
+#     optimizer='adam',
+#     loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+#     metrics=['accuracy'])
 
-model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=3
-)
+# model.fit(
+#     train_ds,
+#     validation_data=val_ds,
+#     epochs=3
+# )
