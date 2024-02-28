f8649abfa25fbd3f945916bb8e2aed2a745b3db8
Vanskarner <vanskarner@gmail.com>
2024-02-28 18:38:30 -0500

    update for predictions_2 in cnn_simple

--- classification/cnn_simple/predictions_2.py
+++ classification/cnn_simple/predictions_2.py
@@ -3,17 +3,8 @@ import os
 from typing import cast
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
 import tensorflow_datasets as tfds
-
-# Preparación de data
-data, metadata = tfds.load('cifar10', as_supervised=True, with_info=True)
-data_test = cast(tf.data.Dataset, data['test'])
-categories_tests = cast(list[str], metadata.features['label'].names)
-LOTSIZE = 25
-data_test = data_test.map(
-    map_func=lambda image, label: (tf.cast(image, tf.float32)/255, label)
-).batch(LOTSIZE)
+import tensorflow as tf
 
 # Carga de modelo guardado
 path = os.path.dirname(os.path.abspath(__file__))
@@ -21,32 +12,58 @@ MODEL_NAME = "CIFAR10_Model2.keras"
 filepath = os.path.join(path, MODEL_NAME)
 model: tf.keras.Sequential = tf.keras.models.load_model(filepath=filepath)
 
-# Ejecución de predicciones
-firstBatch = next(iter(data_test))
-images_test, labels_test = firstBatch
-images_test = images_test.numpy()
-labels_test = labels_test.numpy()
-predicctions = model.predict(images_test)
+# Preparación de data
+data, metadata = tfds.load('cifar10', as_supervised=True, with_info=True)
+data_test = cast(tf.data.Dataset, data['test'])
+categories = cast(list[str], metadata.features['label'].names)
+QUANTITY = 12
+data_test = data_test.take(QUANTITY)
+data_test = data_test.map(map_func=lambda image, label: (
+    tf.cast(image, tf.float32)/255, label))
+data_test = data_test.as_numpy_iterator()
+images, label_indexes = zip(*data_test)
+images, label_indexes = np.array(images), np.array(label_indexes)
 
-true_labels = [categories_tests[label] for label in labels_test[:25]]
-predicted_labels = [categories_tests[np.argmax(pred)] for pred in predicctions[:25]]
+# # Ejecución de predicciones
+predicctions = model.predict(images)
+predicted_percentages = [tf.nn.softmax(pred).numpy() for pred in predicctions]
+max_predicted_percentages = [np.amax(pred) for pred in predicted_percentages]
+predicted_label_indexes = [np.argmax(pred) for pred in predicctions]
 
-# Calcular el porcentaje de cada categoría predicha
-total_predictions = len(predicted_labels)
-category_counts = {category: predicted_labels.count(category) for category in categories_tests}
-category_percentages = {category: count / total_predictions * 100 for category, count in category_counts.items()}
+# ------------ Gráfica de la predicción ------------
+rows, columns = (3, 4)
+plt.figure(figsize=(2*2*columns, 2*rows))
+plt.suptitle('Predictions')
+for i in range(QUANTITY):
+    # Gráfico de la imagen y etiquetas
+    real_label = categories[label_indexes[i]]
+    predicted_label = categories[predicted_label_indexes[i]]
+    predicted_percentage = f'{max_predicted_percentages[i]* 100:.2f}'
+    plt.subplot(rows, 2*columns, 2*i+1)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(images[i])
+    COLOR = 'blue' if real_label == predicted_label else 'red'
+    xlabel = (f"{predicted_label} "
+              f"{predicted_percentage}% "
+              f"({real_label})")
+    plt.xlabel(xlabel=xlabel, color=COLOR)
 
-# Mostrar las imágenes junto con las etiquetas verdaderas, predichas y sus porcentajes
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i + 1)
-    plt.imshow(images_test[i])
-    true_label = true_labels[i]
-    predicted_label = predicted_labels[i]
-    percentage = category_percentages[predicted_label]
-    color = 'green' if true_label == predicted_label else 'red'
-    plt.title(f'True: {true_label}\nPredicted: {predicted_label}\nPercentage: {percentage:.2f}%', color=color)
-    plt.axis('off')
+    # Gráfico de barras del arreglo de valores
+    category_indexes = range(len(categories))
+    category_predicted_percentages = predicted_percentages[i]
+    predicted_label_index = predicted_label_indexes[i]
+    real_label_index = label_indexes[i]
+    plt.subplot(rows, 2*columns, 2*i+2)
+    plt.grid(False)
+    plt.xticks(category_indexes)
+    plt.yticks([])
+    bar_plot = plt.bar(
+        category_indexes, category_predicted_percentages, color="#777777")
+    plt.ylim([0, 1])
+    bar_plot[predicted_label_index].set_color('red')
+    bar_plot[real_label_index].set_color('blue')
 
 plt.tight_layout()
 plt.show()
